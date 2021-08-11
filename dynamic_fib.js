d0b92d8c061880535444601e67673f96c163adf8
frmendez21 <frmendez209@gmail.com>
2021-08-10 22:36:32 -0700

    dynamic fib

--- dynamic_fib.js
+++ dynamic_fib.js
@@ -1,9 +1,18 @@
-const dynamicFib = (n, memo={}) => {
+const memoFib = (n, memo={}) => {
     if(memo[n]) return memo[n];
     if(n <= 2) return 1;
 
-    memo[n] = dynamicFib(n - 1, memo) + dynamicFib(n - 2, memo);
+    memo[n] = memoFib(n - 1, memo) + memoFib(n - 2, memo);
     return memo[n]
 };
 
-console.log(dynamicFib(4))
+const tabFib = (n) => {
+    if(n <= 2) return 1;
+    const fibNums = [0, 1, 1];
+    for(let i = 3; i <= n; i++) {
+        fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+    }
+    return fibNums[n]
+}
+console.log(memoFib(4))
+console.log(tabFib(4))
