3ec3dbd658d802c743b47c2a73202ba0bdf2fda3
frmendez21 <frmendez209@gmail.com>
2021-08-15 16:37:01 -0700

    coinchange tab

--- coin_change.js
+++ coin_change.js
@@ -1,13 +1,15 @@
-const coinChange = (coins, value, m=coins.length) => {
-    if(value == 0) return 1;
+const coinChange = (coins, value) => {
+    
+    const table = Array(value + 1).fill(0);
+    table[0] = 1;
 
-    if(value < 0) return 0;
-
-    if(m <= 0 && value >= 1) return 0
-
-    return coinChange(coins, value, m - 1) + coinChange(coins, value - coins[m - 1], m)
+    for(let i = 0; i < coins.length; i++) {
+        for(let j = coins[i]; j <= value; j++) {
+            table[j] += table[j - coins[i]]
+        }
+    }
+    return table[table.length - 1];
 };
 
 const coins = [1, 5, 10, 25];
-console.log(coinChange(coins, 5)) // 2
-console.log(coinChange(coins, 10)) // 4
+console.log(coinChange(coins, 25))
