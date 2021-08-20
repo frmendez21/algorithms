8cea136df13fb16b17759ac5780721361ecd9504
frmendez21 <frmendez209@gmail.com>
2021-08-19 19:59:56 -0700

    trie

--- trie_addwords.js
+++ trie_addwords.js
@@ -30,6 +30,33 @@ class Trie {
         }
         return this;
     }
+
+    findWord(word) {
+        let char = word[0];
+        if(word.length === 0 || !this.characters[char]) {
+            return;
+        } else if(word.length === 1) {
+            return this.characters[word];
+        } else {
+            let subTrie = this.characters[char];
+            return subTrie.findWord(word.slice(1));
+        }
+    }
+
+    getWords(word="", result=[]) {
+        let keys = Object.keys(this.characters);
+        if(this.isWord) {
+            result.push(word)
+            return result;
+        } else if(!this.isWord && keys.length > 0) {
+            for(let c in this.characters) {
+                let subTrie = this.characters[c];
+                word += c;
+                return subTrie.getWords(word, result);
+            }
+        } 
+        // return result;
+    }
 }
 
 const t = new Trie();
@@ -41,5 +68,8 @@ t.addWord('father')
 t.addWord('forget')
 t.addWord('awesome')
 t.addWord('argue')
-t.removeWord('fat')
-console.log(t.characters.f.characters.a.characters.t)
+// t.removeWord('fat')
+// console.log(t.characters.f.characters.a.characters.t.characters.h.characters.e.characters.r)
+// console.log(t.characters.f.characters.a.characters.t)
+// console.log(t.findWord('father'))
+console.log(t.getWords())
