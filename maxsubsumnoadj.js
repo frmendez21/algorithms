198e9b9172cb38c26c67edf85342e137a5b494ab
frmendez21 <frmendez209@gmail.com>
2021-09-10 09:10:41 -0700

    maxsubsum

--- maxsubsumnoadj.js
+++ maxsubsumnoadj.js
@@ -2,7 +2,7 @@ function maxSubsetSumNoAdjacent(array) {
   // Write your code here.
 	if(!array.length) return 0;
 	if(array.length < 3) return Math.max(...array)
-	const table = new Array(array.length).fill(0);
+	const table = array.slice();
 	table[1] = Math.max(array[0], array[1]);
 	for(let i = 2; i < table.length; i++) {
 		table[i] = Math.max(table[i - 1], (array[i] + table[i - 2]));
